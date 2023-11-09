91c28657b4acdae70fbb10320dc734f2d02d282d
Az1zbek <122464809+Az1zbekErgashev@users.noreply.github.com>
2023-11-09 12:19:36 +0300

    hello my dier

---
+++ TeamProjectMVC/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TeamProjectMVC.Repository;
+
+namespace TeamProjectMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly  IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        [Authorize(Roles = "ADMIN , USER")]
+        public async Task<IActionResult> GetProduct()
+        {
+            var products = await productRepository.GetAll();
+            return View("Product", products);
+        }
+
+    }
+}
--- TeamProjectMVC/Program.cs
+++ TeamProjectMVC/Program.cs
@@ -55,7 +55,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=GetProduct}/{id?}");
 
 app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
 

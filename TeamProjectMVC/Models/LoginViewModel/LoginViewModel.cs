6b8a7acece7a0d49236fdd7a94185470086c1259
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2023-11-11 15:01:57 +0500

    Login problems fixed

--- TeamProjectMVC/Models/LoginViewModel/LoginViewModel.cs
+++ TeamProjectMVC/Models/LoginViewModel/LoginViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using TeamProjectMVC.ValidationAttributes;
 
 namespace TeamProjectMVC.Models.LoginViewModel
 {
     public class LoginViewModel
     {
-        
-     
+
+        [Required(ErrorMessage = "Email is required.")]
+        [CustomEmail(ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Email or password is incorrect.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Email or password is incorrect.")]
         public string Password { get; set; }
     }
 }
